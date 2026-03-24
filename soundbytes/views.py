a633a955f6ba57e8e30c98fd7af516188cc206b2
astraex5356 <leiagardiner2308@gmail.com>
2026-03-24 22:19:50 +0000

    updated tag and genre (plus other bugs)

--- soundbytes/views.py
+++ soundbytes/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse, FileResponse
 from .functions.file_manager import create_song
 from .functions.search_function import search, sort_songs
 from django.views.decorators.csrf import csrf_exempt
-from .models import Song
+from .models import Song, Genre
 
 @csrf_exempt
 def upload_song(request):
     if request.method == 'POST':
         song = create_song(request.POST, request.FILES)
-        return JsonResponse({'status': 'success', 'song_id': song.id})
+        genre_ids = request.POST.getlist('genres')
+        song.genres.set(genre_ids)
+        return redirect('/search-page/')
     return JsonResponse({'error': 'Invalid request'})
 
 @csrf_exempt
@@ -21,15 +23,19 @@ def search_songs(request):
     return JsonResponse(list(songs.values()), safe=False)
 
 def upload_page(request):
-    return render(request, 'soundbytes_auth/upload.html')
+    genres = Genre.objects.all()
+    return render(request, 'soundbytes_auth/upload.html', {'genres': genres})
 
 def search_page(request):
     query = request.GET.get('q', '')
     genre = request.GET.get('genre')
     songs = search(query)
     if genre:
-        songs = songs.filter(genre=genre)
-    return render(request, 'soundbytes_auth/search.html', {'songs': songs})
+        songs = songs.filter(genres__name=genre)
+    genres = Genre.objects.all()
+    sort_by = request.GET.get('sort', '')
+    songs = sort_songs(songs, sort_by)
+    return render(request, 'soundbytes_auth/search.html', {'songs': songs, 'genres': genres})
 
 def stream_song(request, song_id):
     song = Song.objects.get(id=song_id)
