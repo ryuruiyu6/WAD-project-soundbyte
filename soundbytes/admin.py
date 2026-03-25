b6f5b702b38685fb8b0b08d53d42eccf49040276
astraex5356 <leiagardiner2308@gmail.com>
2026-03-25 04:20:22 +0000

    added album system

--- soundbytes/admin.py
+++ soundbytes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Song, Genre
+from .models import Song, Genre, Album
 
 admin.site.register(Song)
 admin.site.register(Genre)
+admin.site.register(Album)
